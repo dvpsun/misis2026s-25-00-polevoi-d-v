4f920d7045e31ab0475eb90079444486bb004ed3
DvpSun <dvpsun@gmail.com>
2026-02-14 09:48:52 +0300

    upd: gitignore and initial test

---
+++ prj.labs/bitsetd/bitsetd.hpp
@@ -0,0 +1,16 @@
+// 2026 by Dmitry Polevoy 
+
+#pragma once
+#ifndef BITSETD_BITSETD_HPP_20260214
+#define BITSETD_BITSETD_HPP_20260214
+
+#include <cstdint>
+
+class BitsetD {
+public:
+  std::int32_t size() const {
+    return 1;
+  }
+};
+
+#endif
