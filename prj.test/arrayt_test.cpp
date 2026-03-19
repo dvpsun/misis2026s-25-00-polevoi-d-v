bedc96511ba110ae8397e1808d02aabeef53b245
DvpSun <dvpsun@gmail.com>
2026-03-19 12:04:29 +0300

    new: arrayt

---
+++ prj.test/arrayt_test.cpp
@@ -0,0 +1,9 @@
+#include <arrayt/arrayt.hpp>
+
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+TEST_CASE("[arrayr] - ctor default") {
+  ArrayT a;
+  CHECK(0 == a.size());
+}
