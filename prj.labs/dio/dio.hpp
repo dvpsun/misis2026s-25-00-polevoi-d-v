c96b11667c860dde49277537eb64c7d27f95e85d
DvpSun <dvpsun@gmail.com>
2026-04-11 11:18:02 +0300

    upd: dio library

--- prj.labs/dio/dio.hpp
+++ prj.labs/dio/dio.hpp
@@ -5,4 +5,52 @@
 #ifndef DIO_DIO_HPP_20260402
 #define DIO_DIO_HPP_20260402
 
+#include <string>
+#include <iosfwd>
+
+class DioStrB {
+public:
+  DioStrB() = default;
+  DioStrB(const DioStrB&) = default;
+  DioStrB(DioStrB&&) = default;
+  DioStrB& operator=(const DioStrB&) = default;
+  DioStrB& operator=(DioStrB&&) = default;
+  ~DioStrB() = default;
+  std::string& val() { return str_; }
+  const std::string& val() const { return str_; }
+  std::ostream& write_to(std::ostream& strm) const;
+  std::istream& read_from(std::istream& istrm);
+
+private:
+  std::string str_;
+  static const char mrk_ = 's';
+};
+
+std::ostream& operator<<(std::ostream& strm, const DioStrB& str);
+
+std::istream& operator>>(std::istream& istrm, DioStrB& str);
+
+
+class DioStrT {
+public:
+  DioStrT() = default;
+  DioStrT(const DioStrT&) = default;
+  DioStrT(DioStrT&&) = default;
+  DioStrT& operator=(const DioStrT&) = default;
+  DioStrT& operator=(DioStrT&&) = default;
+  ~DioStrT() = default;
+  std::string& val() { return str_; }
+  const std::string& val() const { return str_; }
+  std::ostream& write_to(std::ostream& strm) const;
+  std::istream& read_from(std::istream& istrm);
+
+private:
+  std::string str_;
+  static const char mrk_ = 's';
+};
+
+std::ostream& operator<<(std::ostream& strm, const DioStrT& str);
+
+std::istream& operator>>(std::istream& istrm, DioStrT& str);
+
 #endif
--- prj.labs/dio/diostrb.cpp
+++ prj.labs/dio/diostrb.cpp
@@ -2,3 +2,58 @@
 // ÌÈÑÈÑ 2026 âåñíà êóğñ ÎÎÏ
 
 #include "dio.hpp"
+#include <iostream>
+
+
+std::ostream& operator<<(std::ostream& ostrm, const DioStrB& str) {
+  return str.write_to(ostrm);
+}
+
+
+std::istream& operator>>(std::istream& istrm, DioStrB& str) {
+  return str.read_from(istrm);
+}
+
+
+std::ostream& DioStrB::write_to(std::ostream& ostrm) const {
+  int32_t size = str_.size();
+  size += 1;
+  ostrm.put(mrk_);
+  ostrm.write(reinterpret_cast<char*>(&size), sizeof(size));
+  ostrm.write(str_.data(), size);
+  return ostrm;
+}
+
+
+std::istream& DioStrB::read_from(std::istream& istrm) {
+  char mark = istrm.get();
+  if (mrk_ == mark) {
+    int32_t size = 0;
+    istrm.read(reinterpret_cast<char*>(&size), sizeof(size));
+    str_.resize(size - 1);
+    istrm.read(str_.data(), size);
+  } else {
+    istrm.setstate(std::ios::badbit);
+  }
+  return istrm;
+}
+
+std::ostream& operator<<(std::ostream& ostrm, const DioStrT& str) {
+  return str.write_to(ostrm);
+}
+
+
+std::istream& operator>>(std::istream& istrm, DioStrT& str) {
+  return str.read_from(istrm);
+}
+
+std::ostream& DioStrT::write_to(std::ostream& ostrm) const {
+
+  return ostrm;
+}
+
+
+std::istream& DioStrT::read_from(std::istream& istrm) {
+
+  return istrm;
+}
